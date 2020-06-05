08d460137d299e58f13390c0f7f0b84e5df7a80e
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2020-06-05 09:28:15 -0500

    Initial commit

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,1 @@
+import '../stylesheet/aplication.scss';
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './public/assets/javascript/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
