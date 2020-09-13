71325a7045f3eafd0f68623af43ef06c11f7892d
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2020-09-12 21:24:03 -0500

    Add stimulus

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,2 +1,8 @@
 import '../stylesheet/aplication.scss';
 import '@fortawesome/fontawesome-free/js/all';
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
