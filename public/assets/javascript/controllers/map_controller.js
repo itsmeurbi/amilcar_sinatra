5b455ea4b7e3bd760d1b11ac5f171d39da848da6
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2020-09-30 22:08:24 -0500

    Create map controller

---
+++ public/assets/javascript/controllers/map_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from 'stimulus';
+import { Loader } from 'google-maps';
+
+const localPosition = { lat: 19.247233, lng: 103.723681 };
+
+export default class extends Controller{
+  static targets = ['mapContainer']
+
+  async initialize() {
+    const { googleKey } = document.body.dataset;
+    const loader = new Loader(googleKey);
+    await loader.load();
+    this.initializeMap();
+  }
+
+  initializeMap() {
+    const mapOptions = { center: localPosition };
+    this.map = this.buildMap(this.mapContainerTarget, mapOptions);
+    this.marker = this.buildMarker(this.map, localPosition);
+  }
+
+  buildMap(container, options) {
+    return new google.maps.Map(container, {
+      zoom: 16,
+      controlSize: 20,
+      ...options,
+    });
+  }
+
+  buildMarker(map, position) {
+    return new google.maps.Marker({
+      map,
+      position,
+    });
+  }
+}
